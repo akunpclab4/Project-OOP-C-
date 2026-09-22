c9a704cabdae450df4b06ffc8b6e52d925ca9c14
akunpclab4 <akunpc.rh12345@gmail.com>
2026-09-22 11:44:51 +0700

    Add files via upload

---
+++ new project c#/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Configuration.Assemblies;
+
+public class HelloWorld
+{
+    String halo = "halo";
+    int angkaSatu;
+    int angkaDua;
+
+    public void fungsiDalamKelas()
+    {
+        String haloJuga = "haloo";
+    }
+
+    public void operasiaTambah(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaDuaPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine("Hasil Tambah: " + (angkaSatuPar + angkaDuaPar));
+
+    }
+
+     public void operasiKurang(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaDuaPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine("Hasil Kurang: " +(angkaSatuPar - angkaDuaPar));
+
+    }
+
+     public void operasiKali(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaDuaPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine("Hasil Perkalian: " + (angkaSatuPar * angkaDuaPar));
+
+    }
+
+     public void operasiBagi(int angkaSatuPar, int angkaDuaPar)
+    {
+        this.angkaSatu = angkaDuaPar;
+        this.angkaDua = angkaDuaPar;
+        Console.WriteLine("Hasil Pembagian: " + (angkaSatuPar / angkaDuaPar));
+
+    }
+
+
+    
+    public static void Main()
+    {
+        Console.WriteLine("Merdeka");
+        HelloWorld inisiasiHello = new HelloWorld();
+
+        Console.WriteLine(inisiasiHello.halo);
+
+        inisiasiHello.fungsiDalamKelas();
+
+        inisiasiHello.operasiaTambah(2,1);
+        inisiasiHello.operasiKurang(4, 2);
+        inisiasiHello.operasiKali(3, 2);
+        inisiasiHello.operasiBagi(4, 2);
+    }
+    
+}
